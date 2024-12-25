1d9471ed6e9d14d186bc43703c61f4ae6afb9d29
Konstantin Velev <konstantin.velev@accedia.com>
2024-12-25 14:25:49 +0200

    Adds new class Person

---
+++ juniors-world-api/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace juniors_world_api
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
